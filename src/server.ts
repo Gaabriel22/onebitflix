ef49ce0dc5010351ce1bb11db315a577aa548e30
Gabriel Amaral <gabrielvieira2205@gmail.com>
2024-06-06 09:46:52 -0300

    Incluindo a primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { sequelize } from './database'
 import { adminJs, adminJsRouter } from './adminjs'
+import { router } from './routes'
 
 const app = express()
 
@@ -8,13 +9,15 @@ app.use(express.static('public'))
 
 app.use(adminJs.options.rootPath, adminJsRouter)
 
+app.use(router)
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     sequelize.authenticate().then(() => {
         console.log('DB connection successfull')
     })
-    
+
     console.log(`Server started successfuly at port ${PORT}`)
 
 })
